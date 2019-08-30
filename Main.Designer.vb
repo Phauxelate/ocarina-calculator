554f3e2b32512a137904a7ee9f6a2e8ecdc01243
Ethan Strike <estrike@cogitoautomation.com>
2019-08-30 15:35:07 -0500

    Added functional program

---
+++ Main.Designer.vb
@@ -0,0 +1,257 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.equations = New System.Windows.Forms.Button()
+        Me.helpBttn = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.thickTxt = New System.Windows.Forms.TextBox()
+        Me.volTxt = New System.Windows.Forms.TextBox()
+        Me.holeDiamTxt = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.reset = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Calculate = New System.Windows.Forms.Button()
+        Me.fippleDiamTxt = New System.Windows.Forms.TextBox()
+        Me.desiredFreqTxt = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.baseFreqTxt = New System.Windows.Forms.TextBox()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'equations
+        '
+        Me.equations.Location = New System.Drawing.Point(129, 221)
+        Me.equations.Name = "equations"
+        Me.equations.Size = New System.Drawing.Size(100, 23)
+        Me.equations.TabIndex = 22
+        Me.equations.Text = "Equations Used"
+        Me.equations.UseVisualStyleBackColor = True
+        '
+        'helpBttn
+        '
+        Me.helpBttn.Location = New System.Drawing.Point(18, 221)
+        Me.helpBttn.Name = "helpBttn"
+        Me.helpBttn.Size = New System.Drawing.Size(51, 23)
+        Me.helpBttn.TabIndex = 21
+        Me.helpBttn.Text = "Help"
+        Me.helpBttn.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.thickTxt)
+        Me.Panel2.Controls.Add(Me.volTxt)
+        Me.Panel2.Controls.Add(Me.holeDiamTxt)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label10)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Location = New System.Drawing.Point(12, 115)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(222, 100)
+        Me.Panel2.TabIndex = 19
+        '
+        'thickTxt
+        '
+        Me.thickTxt.Location = New System.Drawing.Point(117, 54)
+        Me.thickTxt.Name = "thickTxt"
+        Me.thickTxt.Size = New System.Drawing.Size(100, 20)
+        Me.thickTxt.TabIndex = 18
+        '
+        'volTxt
+        '
+        Me.volTxt.Location = New System.Drawing.Point(117, 32)
+        Me.volTxt.Name = "volTxt"
+        Me.volTxt.Size = New System.Drawing.Size(100, 20)
+        Me.volTxt.TabIndex = 17
+        '
+        'holeDiamTxt
+        '
+        Me.holeDiamTxt.Location = New System.Drawing.Point(117, 76)
+        Me.holeDiamTxt.Name = "holeDiamTxt"
+        Me.holeDiamTxt.Size = New System.Drawing.Size(100, 20)
+        Me.holeDiamTxt.TabIndex = 16
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(3, 6)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Results:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(3, 79)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(106, 13)
+        Me.Label10.TabIndex = 14
+        Me.Label10.Text = "Open Hole Diameter:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(3, 35)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Volume: "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(3, 57)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(62, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Thickness: "
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.reset)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Calculate)
+        Me.Panel1.Controls.Add(Me.fippleDiamTxt)
+        Me.Panel1.Controls.Add(Me.desiredFreqTxt)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.baseFreqTxt)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(222, 108)
+        Me.Panel1.TabIndex = 20
+        '
+        'reset
+        '
+        Me.reset.Location = New System.Drawing.Point(7, 74)
+        Me.reset.Name = "reset"
+        Me.reset.Size = New System.Drawing.Size(75, 23)
+        Me.reset.TabIndex = 19
+        Me.reset.Text = "Restart"
+        Me.reset.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Fipple Diameter:"
+        '
+        'Calculate
+        '
+        Me.Calculate.Location = New System.Drawing.Point(142, 74)
+        Me.Calculate.Name = "Calculate"
+        Me.Calculate.Size = New System.Drawing.Size(75, 23)
+        Me.Calculate.TabIndex = 6
+        Me.Calculate.Text = "Calculate"
+        Me.Calculate.UseVisualStyleBackColor = True
+        '
+        'fippleDiamTxt
+        '
+        Me.fippleDiamTxt.Location = New System.Drawing.Point(117, 3)
+        Me.fippleDiamTxt.Name = "fippleDiamTxt"
+        Me.fippleDiamTxt.Size = New System.Drawing.Size(100, 20)
+        Me.fippleDiamTxt.TabIndex = 1
+        '
+        'desiredFreqTxt
+        '
+        Me.desiredFreqTxt.Location = New System.Drawing.Point(117, 48)
+        Me.desiredFreqTxt.Name = "desiredFreqTxt"
+        Me.desiredFreqTxt.Size = New System.Drawing.Size(100, 20)
+        Me.desiredFreqTxt.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Base Frequency:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Next Note Frequency:"
+        '
+        'baseFreqTxt
+        '
+        Me.baseFreqTxt.Location = New System.Drawing.Point(117, 26)
+        Me.baseFreqTxt.Name = "baseFreqTxt"
+        Me.baseFreqTxt.Size = New System.Drawing.Size(100, 20)
+        Me.baseFreqTxt.TabIndex = 3
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(240, 253)
+        Me.Controls.Add(Me.equations)
+        Me.Controls.Add(Me.helpBttn)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Main"
+        Me.Text = "Main"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents equations As Button
+    Friend WithEvents helpBttn As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents thickTxt As TextBox
+    Friend WithEvents volTxt As TextBox
+    Friend WithEvents holeDiamTxt As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents reset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Calculate As Button
+    Friend WithEvents fippleDiamTxt As TextBox
+    Friend WithEvents desiredFreqTxt As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents baseFreqTxt As TextBox
+End Class
---
+++ Main.vb
@@ -0,0 +1,273 @@
+﻿Imports System.IO
+
+Public Class Main
+
+#Region "Variables"
+    Protected holes As New ArrayList
+    Protected baseVolume As Double
+    Protected thickness As Double
+    Protected baseFrequency As Double
+    Protected fippleDiam As Double
+    Protected previousFreq As Double
+    Protected firstTime As Boolean
+    Protected numberOfHoles As Integer
+#End Region
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        firstTime = True
+    End Sub
+
+    ''' <summary>
+    ''' Calculates volume of an Ocarina.
+    ''' </summary>
+    ''' <param name="F">The base frequency.</param>
+    ''' <param name="D">The diameter of the Fipple</param>
+    ''' <returns>DOUBLE - The Volume of the Ocarina.</returns>
+    Private Function CalculateVolume(F As Double, D As Double) As Double
+
+        Dim V As Double = 1 / ((Math.Pow((F / 2148.14), 2)) / D)
+        Return V
+    End Function
+
+    ''' <summary>
+    '''  Calculates total thickness of the clay.
+    ''' </summary>
+    ''' <param name="V">The volume of the Ocarina.</param>
+    ''' <param name="F">The base frequency of the Ocarina.</param>
+    ''' <param name="D">The diameter of the fipple.</param>
+    ''' <returns>DOUBLE - Thickness of the Ocarina.</returns>
+    Private Function CalculateTotalThickness(V As Double, F As Double, D As Double) As Double
+
+        'Converts the diameter to Area
+        Dim A = (Math.PI * Math.Pow((D / 2), 2))
+
+        'Calculates the thickness
+        Dim T As Double = ((A / 4) * Math.Pow(13503.9 / (2 * Math.PI), 2)) / (((Math.Pow(F, 2)) / 4) * V)
+
+        Return T
+    End Function
+
+    ''' <summary>
+    ''' Calculates total open area on the Ocarina.
+    ''' </summary>
+    ''' <param name="V">Volume of the Ocarina.</param>
+    ''' <param name="T">Thickness of the Ocarina.</param>
+    ''' <param name="F">Frequency of the Ocarina.</param>
+    ''' <returns>DOUBLE - Total open area of the Ocarina.</returns>
+
+    Private Function CalculateTotalOpenArea(V As Double, T As Double, F As Double) As Double
+
+        Dim totalArea = (((Math.Pow(F, 2) / 4 * V * T)) / Math.Pow((13503.9 / (2 * Math.PI)), 2))
+
+        Return totalArea
+    End Function
+
+    ''' <summary>
+    ''' Calculates the diameter for a hole.
+    ''' </summary>
+    ''' <param name="A">Area of the hole.</param>
+    ''' <returns>DOUBLE - The area of the hole.</returns>
+    Private Function CalculateDiameter(A As Double) As Double
+        Dim area = Math.Sqrt((A * 4) / Math.PI)
+
+        Return area
+    End Function
+
+    ''' <summary>
+    ''' Calculates the frequency of a note by the number of steps it takes.
+    ''' </summary>
+    ''' <param name="f0">The base note frequency.</param>
+    ''' <param name="n">The number of steps higher the note to calculate is.</param>
+    ''' <returns>DOUBLE - The frequency of the new note.</returns>
+    Function CalculateStepFrequency(ByVal f0 As Double, ByVal n As Double) As Double
+        Dim a As Double = Math.Pow(2, (1 / 12))
+        Dim fn As Double
+
+        ' Add up numbers.
+        fn = f0 * Math.Pow(a, n)
+
+        Return fn
+    End Function
+
+    ''' <summary>
+    ''' This calculates the Ocarina hole diameter.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Calculate.Click
+        Try
+            'Sets the global variables to the text box values
+            baseVolume = CType(CalculateVolume(CType(baseFreqTxt.Text, Double), CType(fippleDiamTxt.Text, Double)), Double)
+            fippleDiam = CType(fippleDiamTxt.Text, Double)
+            thickness = CType(CalculateTotalThickness(baseVolume, CType(baseFreqTxt.Text, Double), CType(fippleDiamTxt.Text, Double)), Double)
+            baseFrequency = CType(baseFreqTxt.Text, Double)
+            volTxt.Text = baseVolume.ToString
+            thickTxt.Text = thickness.ToString
+
+
+            'Creates temp variables to store the hole area and diameter
+            Dim targetHoleArea As Double
+            Dim targetHoleDiam As Double
+
+            'If this is the first time the program is running, add the fipple diameter to the holes array
+            If firstTime Then
+                holes.Add(CalculateTotalOpenArea(baseVolume, thickness, baseFrequency))
+                targetHoleArea = holes.Item(holes.Count - 1)
+                targetHoleDiam = CalculateDiameter(targetHoleArea)
+
+                previousFreq = baseFrequency
+                firstTime = False
+            End If
+
+            'Adds the hole to the array
+            holes.Add(CalculateTotalOpenArea(baseVolume, thickness, CType(desiredFreqTxt.Text, Double)) - CalculateTotalOpenArea(baseVolume, thickness, previousFreq))
+
+            'Calculates the diameter of the new hole
+            targetHoleArea = holes.Item(holes.Count - 1)
+            targetHoleDiam = CalculateDiameter(targetHoleArea)
+
+            'Sets the text box to display the hole diameter
+            previousFreq = desiredFreqTxt.Text
+            holeDiamTxt.Text = targetHoleDiam.ToString
+
+            'Increases the number of holes counter
+            numberOfHoles += 1
+
+            'Sets the text file name to store the measurements
+            Dim strFile As String = "Ocarina Measurements.txt"
+
+            'If the file doesn't exist, create it and add the base measurements
+            If Not File.Exists("Ocarina Measurements.txt") Then
+                Using sw As New StreamWriter(File.Open(strFile, FileMode.Create))
+                    sw.WriteLine("Fipple Diameter: " + fippleDiam.ToString)
+                    sw.WriteLine("Volume: " + baseVolume.ToString)
+                    sw.WriteLine("Thickness: " + thickness.ToString)
+                    sw.WriteLine("Base Frequency: " + baseFrequency.ToString)
+                    sw.WriteLine("")
+                End Using
+            End If
+
+            'Open the text file and write the newly generated hole diameter into it
+            Using sw As New StreamWriter(strFile, True)
+                sw.WriteLine("Hole " + numberOfHoles.ToString + " Diameter: " + targetHoleDiam.ToString)
+            End Using
+
+        Catch ex As InvalidCastException
+            'Creates a temporary variable as a double
+            Dim temp As Double
+
+            'Checks if the text box is a number. If it isn't, handle the case whether it's missing or a character
+            If Not Double.TryParse(fippleDiamTxt.Text, temp) Then
+                'If the text box is not a number
+                If fippleDiamTxt.Text <> "" And fippleDiamTxt.Text <> "MISSING!" Then
+                    fippleDiamTxt.Text = "NUMBERS ONLY!"
+                End If
+                'If the text box is empty
+                If fippleDiamTxt.Text = "" Then
+                    fippleDiamTxt.Text = "MISSING!"
+                End If
+            End If
+
+            'Checks if the text box is a number. If it isn't, handle the case whether it's missing or a character
+            If Not Double.TryParse(baseFreqTxt.Text, temp) Then
+                'If the text box is not a number
+                If baseFreqTxt.Text <> "" And baseFreqTxt.Text <> "MISSING!" Then
+                    baseFreqTxt.Text = "NUMBERS ONLY!"
+                End If
+                'If the text box is empty
+                If baseFreqTxt.Text = "" Then
+                    baseFreqTxt.Text = "MISSING!"
+                End If
+            End If
+
+            'Checks if the text box is a number. If it isn't, handle the case whether it's missing or a character
+            If Not Double.TryParse(desiredFreqTxt.Text, temp) Then
+                'If the text box is not a number
+                If desiredFreqTxt.Text <> "" And desiredFreqTxt.Text <> "MISSING!" Then
+                    desiredFreqTxt.Text = "NUMBERS ONLY!"
+                End If
+                'If the text box is empty
+                If desiredFreqTxt.Text = "" Then
+                    desiredFreqTxt.Text = "MISSING!"
+                End If
+            End If
+
+
+
+        End Try
+
+    End Sub
+
+    ''' <summary>
+    ''' This button shows a window of how to use the program properly.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub HelpBttn_Click(sender As Object, e As EventArgs) Handles helpBttn.Click
+        MessageBox.Show("This program calculates the diameter of finger holes for an ocarina (or possibly some other similar instrument). I have not found a single source that showed how to do this either mathematically or programatically, so I decided figure it out myself.
+
+All units are in inches and hertz.
+
+To start, enter a desired fipple diameter. All other measurements are based off of this value, so you'll need to choose something semi random for this. For an AC ocarina, this could be around 0.15 inches.
+
+Next, enter the base frequency of the ocarina. This is the frequency where every finger hole is closed.
+
+Now here's the funky part: The way the diameter of holes are calculated is by the total area of open holes. So to start calculating the holes, you'll need to choose a note higher than what you want in order for it to be physically open. When you calculate the last hole (On a 12 hole AC Ocarina this would be F with a frequency of 1396.91 Hz), that hole will be open if you try to physically play the note, so you won't need a higher note frequency for it.
+
+After each caluclation, the program spits out a text file containing the measurements in the same folder so you won't have to manually record them.
+
+If you add in a number wrong, you have to press restart and try again. The hole diameters are linked to eachother, which means you can't just ignore that you just added an unwanted hole into your measurement calculations!
+
+
+Created by Ethan Strike, August 30th, 2019.")
+    End Sub
+
+    ''' <summary>
+    ''' This button shows the equations used in the program, or other equations that never got used but still work.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Equations_Click(sender As Object, e As EventArgs) Handles equations.Click
+        MessageBox.Show("V = Volume
+A = Area
+F = Frequency
+D = Diameter
+L = Length
+W = Width
+
+Calculate Frequency (from Diameter and Volume) = 2148.14 * ((D / V)^(1/2))
+Calculate Volume (from Frequency and Diameter) = 1 / ((((F / 2148.14)^2)) / D)
+Calculate Diameter (from Length and Width) = 2 * (((l * w) / Math.PI)^(1/2))
+Calculate Diameter (from Volume and Frequency) =  ((F / 2148.14)^2) * V
+Calculate Thickness (from Volume, Frequency, and Area) = ((A / 4) * (13503.9 / (2 * PI)^2)) / (((F^2) / 4) * V)
+Calculate Total Area (from Volume, Thickness, Frequency) = (((F^2) / 4 * V * T)) / ((13503.9 / (2 * Math.PI))^2)
+Calculate Diameter (from Area of a Hole) = Math.sqrt((A * 4) / PI)
+Calculate Area of Fipple (from Width and Circular Fipple Diameter) = (PI * ((D/2)^2))/W")
+    End Sub
+
+    ''' <summary>
+    ''' This button resets the window in case someone funks up inputting numbers.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles reset.Click
+        'Wipes all the variables
+        holes.Clear()
+        baseVolume = 0
+        thickness = 0
+        baseFrequency = 0
+        fippleDiam = 0
+        previousFreq = 0
+        firstTime = 0
+        numberOfHoles = 0
+
+        'Checks to see if a text file has already been generated. If it has, delete it
+        If File.Exists("Ocarina Measurements.txt") Then
+            File.Delete("Ocarina Measurements.txt")
+        End If
+
+        Me.Controls.Clear() 'Removes all the controls on the form
+        InitializeComponent() 'Load all the controls again
+        Main_Load(e, e) 'Load everything in your form load event again
+    End Sub
+End Class
